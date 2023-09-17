d81b084912ece20d352ff0a8afa939e7c822154a
TaniaUdod <taniaudod28@gmail.com>
2023-09-18 00:14:02 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector(".feedback-form");
+
+const STORAGE_KEY = "feedback-form-state";
+
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
+
+let formData = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
+const { email, message } = form.elements;
+reloadPage();
+
+function onInput(event) {
+    formData[event.target.name] = event.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function reloadPage() {
+    if (formData) {
+        email.value = formData.email || '';
+        message.value = formData.message || '';
+    }
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    console.log({ email: email.value, message: message.value });
+    
+    localStorage.removeItem(STORAGE_KEY);
+    event.currentTarget.reset();
+}
