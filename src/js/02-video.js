2a630415393fff278a174628e07c94abecfb4ce6
TaniaUdod <taniaudod28@gmail.com>
2023-09-17 17:14:52 +0300

    task 1-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const CURRENT_VIDEO_TIME = "videoplayer-current-time";
+
+const onPlay = function (event) {
+    localStorage.setItem(CURRENT_VIDEO_TIME, event.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(CURRENT_VIDEO_TIME)));
